274f24410b600c829522db75d42f1fbad6cb2127
Florencia Cabrera <aflorenciacabrera@gmail.com>
2017-10-23 00:11:17 -0300

    venganza modificacion de inicio de productores

--- resources/views/productor/inicio.blade.php
+++ resources/views/productor/inicio.blade.php
@@ -1,29 +1,32 @@
 @extends('layouts.app')
-
 @section('content')
 
+  <div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                
+              <div class="panel-heading"><h2 class="text-center"> Productores</h2> </div> 
 
+               <div class="panel-body">
+                 <div class="row">
+                        <div class="col-lg-4 col-lg-offset-2 col-md-3 col-md-offset-3 col-sm-4 col-sm-offset-2 col-xs-6">
+                            <a class="btn btn-default btn-block" type="button" href="{{ url('productor/acceso') }}">
+                              <img src="{{asset('img/acceder.png')}}" class="img-circle img-responsive center-block"/> 
+                               <h5 class="text-center">INGRESAR</h5>
+                            </a>
+                        </div>
+                        <div class="col-lg-4 col-md-3 col-sm-4 col-xs-6">
+                            <a class="btn btn-default btn-block" type="button" href="{{ url('productor/registro') }}">
+                              <img  src="{{asset('img/im5.png')}}"  class="img-circle img-responsive center-block"/>
+                               <h5 class="text-center">CREAR CUENTA</h5>
+                            </a>
+                        </div>
+                 </div>
+              </div>
+            </div>
 
-
-<div class="panel panel-default">
-    <div class="panel-body">
-        <h1 class="text-center">Pequeños Productores</h1>
-       <div class="row">
-    <div class="col-lg-3 col-lg-offset-3 col-md-3 col-md-offset-3 col-sm-4 col-sm-offset-2 col-xs-6">
-        <a class="btn btn-default btn-block" type="button" href="{{ url('productor/acceso') }}">
-          <img src="{{asset('img/acceder.png')}}" class="img-circle img-responsive center-block"/> 
-           <h3 class="text-center">INGRESAR</h3>
-        </a>
+        </div>
     </div>
-    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
-        <a class="btn btn-default btn-block" type="button" href="{{ url('productor/registro') }}">
-          <img  src="{{asset('img/im5.png')}}"  class="img-circle img-responsive center-block"/>
-           <h3 class="text-center">CREAR CUENTA</h3>
-        </a>
-    </div>
-    </div>
-    </div>
-</div>
-
-
+  </div>
 @endsection
