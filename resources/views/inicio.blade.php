af58124457d03365282c72c1d66dca72e7f4fcaf
Ivan Sambrana <sambranaivan@gmail.com>
2017-10-25 15:17:14 -0300

    home

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -7,29 +7,44 @@
 
 
 
-    <div class="container">
-          <h1 class= "text-center">  Oportunidades Laborales </h1>
-               
-        <div class="carousel slide" data-ride="carousel" id="carousel-1">
+   <div class="container">
 
-            <div class="carousel-inner" role="listbox">
-                <div class="item active"><img src="http://placeholdit.imgix.net/~text?txtsize=42&amp;txt=Carousel+Image&amp;w=1400&amp;h=600" alt="Slide Image"></div>
-                <div class="item"><img src="http://placeholdit.imgix.net/~text?txtsize=42&amp;txt=Carousel+Image&amp;w=1400&amp;h=600" alt="Slide Image"></div>
-                <div class="item"><img src="http://placeholdit.imgix.net/~text?txtsize=42&amp;txt=Carousel+Image&amp;w=1400&amp;h=600" alt="Slide Image"></div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="row">
+                        <a class="btn btn-lg btn-primary btn-block btn-responsive">
+                            <h2>20 Instituciones Registradas</h2>
+                        </a>
+                    </div>
+                </div>
+               <div class="col-md-6">
+                    <div class="row">
+                        <a class="btn btn-lg btn-info btn-block">
+                            <h2>40 Productores Activos</h2>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <a class="btn btn-lg btn-danger btn-block">
+                            <h2>+100 Oportunidades Laborales Publicadas</h2>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <a class="btn btn-lg btn-success btn-block">
+                            <h2>300 Capacidades Disponibles</h2>
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div><a class="left carousel-control" href="#carousel-1" role="button" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i><span class="sr-only">Previous</span></a><a class="right carousel-control" href="#carousel-1" role="button"
-                data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i><span class="sr-only">Next</span></a></div>
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-1" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-1" data-slide-to="1"></li>
-                <li data-target="#carousel-1" data-slide-to="2"></li>
-            </ol>
         </div>
     </div>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</div>
+   
           
 
-        </div>
+   
 
 @endsection
