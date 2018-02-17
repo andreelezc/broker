559d9ab602065d6d61bae6b35d4cce4af3243ae6
Florencia Cabrera <aflorenciacabrera@gmail.com>
2018-02-16 23:06:32 -0300

    formulario de capacidad

--- resources/views/institucion/capacidad.blade.php
+++ resources/views/institucion/capacidad.blade.php
@@ -31,17 +31,87 @@ $(document).ready(function(){
 
             {{-- TITULO --}}
             <div class="form-group">
-                <label class="control-label" for="text-input"> Título: <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Ingrese un titulo acorde a su Capacidad"></span></label>
-                <input class="form-control" type="text" name="titulo" id="text-input"  data-toggle="tooltip"  >
+                <div class="col-md-2 col-md-offset-0 col-sm-2 label-column">
+                    <label class="control-label" for="text-input"> Título: <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Ingrese un titulo acorde a su Capacidad"></span></label>
+                </div>
+                <div class="col-md-9 col-md-offset-0 col-sm-6 input-column">
+                    <input class="form-control" type="text" name="titulo" id="text-input"  data-toggle="tooltip" placeholder="Ingresar Título" >
+                </div>
+                <br>
             </div>
-            
+            <br>
+
             {{-- Descripcion --}}
-            <div class="form-group">
-                <label class="control-label" for="email-input"> Descripción:  <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Describa su capacidad"></span> </label>
-                <textarea class="form-control"  name="descripcion" rows="8" cols="40" id="text-input" placeholder=" Escriba aquí una breve descripción de su capacidad laboral.."></textarea>
+           <div class="form-group">
+                <div class="col-md-2 col-md-offset-0 col-sm-2 label-column">
+                    <label class="control-label" for="text-input"> Descripción:  <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Describa su capacidad"></span> </label>
+                </div>
+                <div class="col-md-9 col-md-offset-0 col-sm-6 input-column">
+                    <textarea class="form-control"  name="descripcion" rows="8" cols="40" id="text-input" placeholder=" Escriba aquí una breve descripción de su capacidad laboral.."></textarea>
+                </div>
+                <br>
             </div>
-            
-                 {{-- PALABRAS CLAVE --}}
+            <br><br><br><br><br><br><br><br>
+          
+        {{-- EXPERIENCIA --}}
+        <div class="form-group">
+            <div class="col-md-2 col-md-offset-0 col-sm-2 label-column">
+                <label class="control-label" for="textarea-input">Experiencias previas:  <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Experias adquiridas que aporten a su capacidad"></span></label>
+            </div>
+            <div class="col-md-9 col-md-offset-0 col-sm-6 input-column">
+                <textarea class="form-control" name="experiencias" rows="8" cols="40" placeholder=" Mencione sus experiencias laborales..." id="textarea-input"></textarea>  
+            </div>
+            <br>
+        </div>
+        <br><br><br><br><br><br><br><br>
+         {{-- CATEGORIA --}}
+         <div class="form-group">
+            <div class="col-md-2 col-md-offset-0 col-sm-2 label-column">
+               <label class="control-label" for="textarea-input">Categoría:   <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Selecionar una categoria o agregar"></span></label>
+            </div>
+            <div class="col-md-9 col-md-offset-0 col-sm-6 input-column">
+                <input class="form-control" type="text" name="categoria" id="text-input" placeholder="Pasante, Trabajo Final, Becas...">
+            </div>
+            <br>
+        </div>
+        <br>
+
+
+        {{-- PERSONAL --}}
+         <div class="form-group">       
+                <div class="col-md-4 col-md-offset-0 col-sm-4 label-column">   
+                    <label class="control-label" for="textarea-input">Cantidad de personal vacante:  <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Personas con esta capacidad adquirida"></span></label>
+                </div>
+                 <div class="col-md-2 col-md-offset-0 col-sm-6 input-column">
+                    <input class="form-control"  type="number"  name="personal" step="1" min="0" max="1000000" placeholder="N° personal"/>
+                </div>
+        </div>
+        
+         {{-- REMUNERACION --}}                    
+       <div class="form-group">
+            <div class="col-md-3 col-md-offset-0 col-sm-4 label-column">
+                <label class="control-label">Remuneración pretendida:   <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Ingresar el importe que se espera recibir acorde a conocimientos y horarios seleccionados"></span></label> 
+            </div>
+            <div class="col-md-2 col-md-offset-0 col-sm-6 input-column">
+               <input class="form-control"  type="number"  name="remuneracion" min="0.00" max="10000.00" step="0.01" placeholder="$" />
+            </div>
+        </div> 
+        <br><br>
+         {{-- Orientado --}}
+         <div class="form-group">
+                <div class="col-md-2 col-md-offset-0 col-sm-2 label-column">
+                   <label class="control-label" for="textarea-input">Orientado a:  <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Especificar a quienes esta orientada la capacidad "></span> </label>
+                </div>
+                <div class="col-md-9 col-md-offset-0 col-sm-6 input-column">
+                   <textarea class="form-control"  name="orientacion" id="text-input" placeholder="PYMES, Grupos, Empresas, Instituciones ..."  rows="5" cols="12" ></textarea>
+                </div>
+                <br>
+            </div>
+
+            <br>
+<br> <br><br> <br>
+                    
+          {{-- PALABRAS CLAVE --}}
         <div class="form-group">
             <div class="row">
                 <div class="col-md-10"> <label class="control-label">Palabras clave:  <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Ingresar 4 palabras diferentes acorde a la capacidad"></span></label>
@@ -62,56 +132,8 @@ $(document).ready(function(){
             </div>
         </div>
 
-        {{-- EXPERIENCIA --}}
-            <div class="form-group">
-                <label class="control-label" for="textarea-input">Experiencias previas:  <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Experias adquiridas que aporten a su capacidad"></span></label>
-                <textarea class="form-control" name="experiencias" rows="8" cols="40" placeholder=" Mencione sus experiencias laborales..." id="textarea-input"></textarea>  
-            </div>
+      
 
-         {{-- PERSONAL --}}
-         <div class="form-group">
-            <div class="row">
-                <div class="col-md-4">    
-                    <label class="control-label" for="textarea-input">Cantidad de personal disponible:  <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Personas con esta capacidad adquirida"></span></label></div>
-                <div class="col-md-2">     
-                    <input class="form-control"  type="number"  name="personal" step="1" min="0" max="1000000" placeholder="N° personal"/>
-                </div>
-             </div>
-        </div>
-
-
-            <div class="row">
-                <div class="form-group">
-                <div class="col-md-4">                   
-                                    
-
-                     {{-- CATEGORIA --}}
-                    <label class="control-label" for="textarea-input">Categoría:   <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Selecionar una categoria o agregar"></span></label>
-                    <input class="form-control" type="text" name="categoria" id="text-input" placeholder="Pasante, Trabajo Final, Becas...">
-                     </div> 
-                    <br>
-                    {{-- REMUNERACION --}}
-                     
-                    <div class="col-md-10">    
-                    <label class="control-label">Remuneración pretendida:   <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Ingresar el importe que se espera recibir acorde a conocimientos y horarios seleccionados"></span></label>
-
-                           
-                     <input class="form-control"  type="number"  name="remuneracion" min="0.00" max="10000.00" step="0.01" placeholder="$" />
-                     </div> 
-                     
-              
-                 
-                </div>
-                 
-            </div>
-
-             {{-- Orientado --}}
-                  
-                 <div class="form-group">
-                         <label class="control-label" for="textarea-input">Orientado a:  <span class="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-original-title="Especificar a quienes esta orientada la capacidad "></span> </label>
-                        <textarea class="form-control"  name="orientacion" id="text-input" placeholder="PYMES, Grupos, Empresas, Instituciones ..."  rows="5" cols="12" ></textarea>
-                    </div>
-                
     {{-- DISPONIBILIDAD --}}
     <div class="row">
         <div class="col-md-8">          
@@ -123,12 +145,14 @@ $(document).ready(function(){
                     <div class="col-md-4 ">
                         <div class="form-group">
                         Fecha de inicio:  <input type="date" placeholder="DD" name="fechaInicio"/>
-                        </div></div>
+                        </div>
+                    </div>
 
                      <div class="col-md-6">
                         <div class="form-group">
                          Fecha de  finalización:  <input type="date" placeholder="DD" name="fechaFin"/>
-                        </div></div>
+                        </div>
+                    </div>
                 </div>
                 
         <table class="table table-user-information">
@@ -213,10 +237,14 @@ $(document).ready(function(){
                    </tbody>
           </table>
                    </div></div></div>
-                  
+        
+          
 
             </div>
         </div>
+
+
+              
      
           
     {{-- REMUNERACION 
