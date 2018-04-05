c3abd5303d0a2cf410d71e31363cf8dbbfa9c098
Florencia Cabrera <aflorenciacabrera@gmail.com>
2018-04-05 15:10:47 -0300

    vista de registro de formulario

--- resources/views/productor/registro.blade.php
+++ resources/views/productor/registro.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
 
                 @if(Session::has('registro'))     
@@ -15,13 +15,16 @@
                 <div class="panel-heading">Registra un Productor</div>
 
                 <div class="panel-body">
+                    <div class="row">
                     <form class="form-horizontal" method="POST" action="{{ url('productor/registro') }}">
                         {{ csrf_field() }}
-
+                        {{--Daros del Productor--}}
+                        <div class="col-md-6">
+                            <div class="panel-heading text-center"><h4>Datos del Productor</h4></div>
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Nombre del Productor</label>
+                            <label for="name" class="col-md-5 control-label">Nombre del Productor</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-7">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
@@ -34,9 +37,9 @@
 
 
                         <div class="form-group{{ $errors->has('direccion') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">Dirección del Productor</label>
+                            <label for="email" class="col-md-5 control-label">Dirección del Productor</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-7">
                                 <input id="direccion" type="text" class="form-control" name="direccion" value="{{ old('direccion') }}" required>
 
                                 @if ($errors->has('direccion'))
@@ -48,9 +51,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('cuit') ? ' has-error' : '' }}">
-                            <label for="cuit" class="col-md-4 control-label"> CUIT:</label>
+                            <label for="cuit" class="col-md-5 control-label"> CUIT:</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-7">
                                 <input id="cuit" type="num" class="form-control" name="cuit" value="{{ old('cuit') }}" required>
 
                                 @if ($errors->has('cuit'))
@@ -64,9 +67,9 @@
 
 
                         <div class="form-group{{ $errors->has('email1') ? ' has-error' : '' }}">
-                            <label for="email1" class="col-md-4 control-label">Correo Electrónico Productor </label>
+                            <label for="email1" class="col-md-5 control-label">Correo Electrónico Productor </label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-7">
                                 <input id="email1" type="email" class="form-control" name="email1" value="{{ old('email1') }}" required>
 
                                 @if ($errors->has('email1'))
@@ -78,9 +81,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Contraseña</label>
+                            <label for="password" class="col-md-5 control-label">Contraseña</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-7">
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -92,24 +95,26 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirmar contraseña</label>
+                            <label for="password-confirm" class="col-md-5 control-label">Confirmar contraseña</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-7">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
 
+                        </div>
 
-                      {{-- Datos de contacto --}}
 
-                        <hr style="width:75%; " >
+                      {{-- Datos de contacto ///  <hr style="width:75%; " > --}}
+                      <div class="col-md-6">
+   
                          <div class="panel-heading text-center"><h4>Datos de contacto</h4></div>
 
                        
                         <div class="form-group{{ $errors->has('name1') ? ' has-error' : '' }}">
-                            <label for="name1" class="col-md-4 control-label text-center">Nombre de contacto:</label>
+                            <label for="name1" class="col-md-5 control-label text-center">Nombre de contacto:</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-7">
                                 <input id="name1" type="text" class="form-control" name="name1" value="{{ old('name1') }}" required autofocus>
 
                                 @if ($errors->has('name1'))
@@ -121,9 +126,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">Correo electrónico de contacto </label>
+                            <label for="email" class="col-md-5 control-label">Correo electrónico de contacto </label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-7">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
@@ -134,9 +139,9 @@
                             </div>
                         </div>
                         <div class="form-group{{ $errors->has('telefono1') ? ' has-error' : '' }}">
-                            <label for="telefono1" class="col-md-4 control-label"> Teléfono de contacto </label>
+                            <label for="telefono1" class="col-md-5 control-label"> Teléfono de contacto </label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-7">
                                 <input id="telefono1" type="num" class="form-control" name="telefono1" value="{{ old('telefono1') }}" required>
 
                                 @if ($errors->has('telefono1'))
@@ -147,9 +152,9 @@
                             </div>
                         </div>
                         <div class="form-group{{ $errors->has('hora') ? ' has-error' : '' }}">
-                            <label for="hora" class="col-md-4 control-label"> Horario de contacto </label>
+                            <label for="hora" class="col-md-5 control-label"> Horario de contacto </label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-7">
                                 <input id="hora" type="text" class="form-control" name="hora" value="{{ old('hora') }}" placeholder="Lunes a Viernes: 8:00 hs - 17:00 hs  " required>
 
                                 @if ($errors->has('hora'))
@@ -158,6 +163,7 @@
                                     </span>
                                 @endif
                             </div>
+                        </div>
                         </div>
 
 
@@ -169,6 +175,7 @@
                             </div>
                         </div>
                     </form>
+                </div>
                 </div>
             </div>
         </div>
