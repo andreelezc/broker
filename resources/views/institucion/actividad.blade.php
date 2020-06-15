b70d5c29d07d6bbcd4f84b9963fe5c8b0aea28d5
Andrea <andreelezc@gmail.com>
2020-06-15 09:29:45 -0300

    modifico graficos

--- resources/views/institucion/actividad.blade.php
+++ resources/views/institucion/actividad.blade.php
@@ -27,7 +27,7 @@
                             <p><h3>OFERTAS</h3></p>
                         </div>
                     </div>
-                    <h2>Capacidades por categoria</h2>
+                    <h2>Capacidades por categoría</h2>
                     <canvas id="myChart" width="400" ></canvas>
                     <h2>Capacidades publicadas recientemente</h2>
                     <canvas id="tiempo" width="400" ></canvas>
@@ -102,34 +102,21 @@
                     labels: @json($label_time),
                     datasets: [{
                         label: 'Cantidad',
+                        fill: false,
                         data: @json($grafico_time),
                         backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)',
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 99, 132, 1)',
                         ],
                         borderColor: [
                             'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)',
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
                         ],
-                        borderWidth: 1
+                        pointBorderColor: [
+                            'rgba(255, 99, 132, 1)',
+                        ],
+                        pointBackgroundColor: [
+                            'rgba(255, 99, 132, 1)',
+                        ],
+                        borderWidth: 3
                     }]
                 },
                 options: {
@@ -153,7 +140,7 @@
                             }
                           }
                         ]
-                    }
+                    },
                 }
             });
             </script>
